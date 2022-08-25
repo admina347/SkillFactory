37c934ad90763268be8e6f8b785e982996e69fbf
admina <it-mactep@mail.ru>
2022-08-25 15:43:24 +0500

    unit6-2-8

--- Program.cs
+++ Program.cs
@@ -1,17 +1,36 @@
-﻿class Pen
+﻿/* Класс должен содержать целочисленные поля для сторон a и b.
+Класс должен содержать метод Square, возвращающий площадь прямоугольника (произведение сторон).
+Класс должен содержать 3 конструктора: с 2 параметрами, когда a != b, с 1 параметром, когда a == b, 
+и конструктор без параметров по умолчанию, в котором стороны будут заполняться как a = 6, b = 4. */
+
+Rectangle Rectangle = new Rectangle(2, 2);
+int s;
+s = Rectangle.Square();
+Console.WriteLine(s);
+
+class Rectangle
 {
-	public string color;
-	public int cost;
+	public int a, b;
+
+    public int Square()
+    {
+        return a * b;
+    }
     // Конструктор 1
-	public Pen()
+	public Rectangle()
 	{
-		color = "Черный";
-		cost = 100;
+		a = 6;
+        b = 4;
 	}
 	// Конструктор 2
-	public Pen(string penColor, int penCost)
+	public Rectangle(int side)
 	{
-		color = penColor;
-		cost = penCost;
+		a = side;
+	}
+	// Конструктор 3
+	public Rectangle(int first, int second)
+	{
+		a = first;
+        b = second;
 	}
 }
