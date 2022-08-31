305702eeeb70df90b419876de7986d3c29e27c43
admina <it-mactep@mail.ru>
2022-08-31 13:18:48 +0500

    unit6.3.2

--- Program.cs
+++ Program.cs
@@ -1,39 +1,28 @@
 ﻿using System;
-class Company
+class Bus
 {
-    public string Type;
-    public string Name;
-}
+    public int? Load;
 
-class Department
-{
-    public Company Company;
-    public City City;
-}
-
-class City
-{
-    public string Name;
+    public void PrintStatus()
+    {
+        if (Load.HasValue)
+        {
+            Console.WriteLine("В авбтобусе {0} пассажиров", Load.Value);
+        }
+        else
+        {
+            Console.WriteLine("Автобус пуст!");
+        }
+    }
 }
 class Program
 {
 
     static void Main(string[] args)
     {
-        var department = GetCurrentDepartment();
-        if (department?.Company?.Type == "Банк" && department?.City?.Name == "Санкт-Петербург")
-        {
-            Console.WriteLine("У банка {0} есть отделение в Санкт-Петербурге", department?.Company?.Name ?? "Неизвестная компания");
-        }
-    }
-
-    static Department GetCurrentDepartment()
-    {
-        City city = new City { Name = "Санкт-Петербург" };
-        Company company = new Company { Type = "Банк", Name = null };
-        Department dep = new Department();
-        dep.Company = company;
-        dep.City = city;
-        return dep;
+        Bus bus = new Bus();
+        bus.PrintStatus();
+        bus.Load = 15;
+        bus.PrintStatus();
     }
 }
