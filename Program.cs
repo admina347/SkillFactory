5b62f84121626d8eb2f22b7e2187a4606576a416
admina <it-mactep@mail.ru>
2022-09-01 13:35:29 +0500

    unit7.1.4

--- Program.cs
+++ Program.cs
@@ -1,73 +1,16 @@
 ﻿using System;
-class User
+class Employee 
 {
-	private int age;
-    private string login;
-    private string email;
-
-	public int Age
-	{
-		get
-		{
-			return age;
-		}
-
-		set
-		{
-			if (value < 18)
-			{
-				Console.WriteLine("Возраст должен быть не меньше 18");
-			}
-			else
-			{
-				age = value;
-			}
-		}
-	}
-    public string Login
-    {
-        get
-        {
-            return login;
-        }
-        set
-        {
-            if(value.Length > 3)
-            {
-                login = value;
-            }
-            else
-            {
-                Console.WriteLine("Поле логина должно быть не менее 3 символов длиной");
-            }
-        }
-    }
-    public string Email
-    {
-        get
-        {
-            return email;
-        }
-        set
-        {
-            if(value.Contains("@"))
-            {
-                email = value;
-            }
-            else
-            {
-                Console.WriteLine("Поле почты должно содержать знак @");
-            }
-        }
-    }
+  public string Name;
+  public int Age;
+  public int Salary;
 }
-class Program
+//Класс ProjectManager должен содержать строковое поле ProjectName, а класс Developer — строковое поле ProgrammingLanguage.
+class ProjectManager : Employee
 {
-
-    static void Main(string[] args)
-    {
-       User u = new User();
-       u.Login = "abcd";
-       u.Email = "s@baka";
-    }
+    public string ProjectName;
+}
+class Developer : Employee
+{
+    private string ProgrammingLanguage;
 }
