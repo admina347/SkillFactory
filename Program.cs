c1d054a788cbf237bdb6fee3fe656c2d35bb34c5
admina <it-mactep@mail.ru>
2022-09-04 14:47:45 +0500

    unit-7.2.7

--- Program.cs
+++ Program.cs
@@ -1,28 +1,52 @@
 ﻿using System;
-
+/* Задание 7.2.7
+Создайте схему классов A, B, C, D и E таким образом, чтобы B наследовался от A, С от A, D от B и E от C. А также:
+Добавьте в класс A виртуальный метод Display (void тип, без параметров), который будет выводить в консоль "A".
+В классе B скройте этот метод и сделайте так, чтобы в консоль выводилось "B".
+Для класса C переопределите метод Display, чтобы в консоли было "C".
+Для D снова скройте метод.
+В классе E также скройте метод.*/
 class Program
 {
-    class BaseClass
+    class A
     {
         public virtual void Display()
         {
-            Console.WriteLine("Метод класса BaseClass");
+            Console.WriteLine("Метод класса A");
         }
     }
-    /* Вернитесь к заданию 7.2.3 и дополните его код так, чтобы для вызова следующего следующего кода в консоль выводилось 2 сообщения 
-    сначала "Метод класса BaseClass", а затем "Метод класса DerivedClass"): */
-    class DerivedClass : BaseClass
+    
+    class B : A
     {
-
+        public new void Display()
+        {
+            Console.WriteLine("Метод класса B");
+        }
+    }
+    class C : A
+    {
         public override void Display()
         {
-            base.Display();
-            Console.WriteLine("Метод класса DerivedClass");
-        }        
+            Console.WriteLine("Метод класса C");
+        }
+    }
+    class D : B
+    {
+        public new void Display()
+        {
+            Console.WriteLine("Метод класса D");
+        }
+    }
+    class E : C
+    {
+        public new void Display()
+        {
+            Console.WriteLine("Метод класса E");
+        }
     }
     static void Main(string[] args)
         {
-            DerivedClass obj = new DerivedClass();
+            A obj = new A();
             
             obj.Display();
         }
