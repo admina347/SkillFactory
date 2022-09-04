28f65c7c467424a394b5d1493f6377a3dd5fc51c
admina <it-mactep@mail.ru>
2022-09-04 13:47:41 +0500

    unit7.2.4

--- Program.cs
+++ Program.cs
@@ -4,31 +4,35 @@ class Program
 {
     class BaseClass
     {
-        public virtual void Display()
+        public virtual int Counter
         {
-            Console.WriteLine("Метод класса BaseClass");
+            get;
+            set;
         }
     }
-    /* Задание 7.2.3
-    Реализуйте в классе BaseClass виртуальный метод Display с типом void и без параметров, 
-    который будет выводить сообщение "Метод класса BaseClass" в консоль, 
-    а затем переопределите его в DerivedClass, чтобы он выводил сообщение "Метод класса DerivedClass". */
+
     class DerivedClass : BaseClass
     {
-
-        public override void Display()
+        private int counter;
+        public override int Counter
         {
-            Console.WriteLine("Метод класса DerivedClass");
+            get
+            {
+                return Counter;
+            }
+            set
+            {
+                if(value >= 0)
+                {
+                    counter = value;
+                }
+            }
         }
-
-        static void Main(string[] args)
-        {
-            BaseClass bc = new BaseClass();
-            DerivedClass dc = new DerivedClass();
-
-            bc.Display();
-            dc.Display();
-            ((BaseClass)dc).Display();
-        }
+        
     }
+    static void Main(string[] args)
+    {
+
+    }
+
 }
