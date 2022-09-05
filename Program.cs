cb1ec5913baacc871a96b46b9945e34480fcc96b
admina <it-mactep@mail.ru>
2022-09-05 20:33:13 +0500

    unit-7.5.3

--- Program.cs
+++ Program.cs
@@ -1,19 +1,24 @@
 ﻿using System;
-/*Задание 7.5.2
-Создайте класс Obj, определите в нем поля Name, Description (тип строки) 
-и статическое поле MaxValue типа int, равное 2000.*/
+/*Задание 7.5.3
+Создайте класс Helper и определите в нем статический метод Swap типа void, который принимает 2 переменные типа int и меняет их значения местами.*/
 class Program
 {
-    class Obj
+    class Helper
     {
-        public string Name;
-
-        public string Description;
-
-        public static int MaxValue = 2000;
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
     }
     static void Main(string[] args)
     {
-
+        int num1 = 3;
+        int num2 = 58;
+        Helper.Swap(ref num1, ref num2);
+        /*передача num1 и num2 в метод*/
+        Console.WriteLine(num1); //58
+        Console.WriteLine(num2); //3
     }
 }
