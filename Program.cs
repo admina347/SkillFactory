08d767dee232c1c328c2b7df35d3591162cf2bd9
admina <it-mactep@mail.ru>
2022-09-06 14:23:53 +0500

    unit-7.6.9

--- Program.cs
+++ Program.cs
@@ -1,19 +1,19 @@
 ﻿using System;
-/*Задание 7.6.7
-Добавьте к схеме классов автомобиля следующие классы частей автомобиля: Battery, Differential, Wheel. 
-Также добавьте в класс Car виртуальный обобщённый метод без реализации — ChangePart, 
-который будет принимать один параметр — newPart универсального типа.*/
+/*адание 7.6.9
+Установите ограничения на универсальные типы в классе Car. Такие, чтобы поле Engine могло принимать тип ElectricEngine и GasEngine, 
+а параметр newPart метода ChangePart мог бы принимать только типы частей машины (Battery, Differential, Wheel).*/
 
-class Car<T1>
+class Car<T1> where T1 : Engine
 {
-    public T1 Engine;
-    public virtual void ChangePart<T2>(T2 newPart)  {}
+    public virtual void ChangePart<T2>(T2 newPart) where T2 : CarPart {}
 }
-class ElectricEngine {}
-class GasEngine {}
-class Battery {}
-class Differential {}
-class Wheel {}
+class ElectricEngine : Engine {}
+class GasEngine : Engine {}
+abstract class Engine {}
+abstract class CarPart {}
+class Battery : CarPart {}
+class Differential : CarPart {}
+class Wheel : CarPart {}
 class Program
 {
     static void Main(string[] args)
