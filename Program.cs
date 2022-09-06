8a23ddfa86b69c5918837485c5d457fd66c13628
admina <it-mactep@mail.ru>
2022-09-06 13:56:56 +0500

    unit-7.6.7

--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-/*Задание 7.6.6
-Реализуйте класс-обобщение Record, у которого будут два универсальных параметра: 
-один — для идентификатора записи (Id), другой — для значения записи (Value). 
-Также в классе реализуйте поле Date типа DateTime.*/
+/*Задание 7.6.7
+Добавьте к схеме классов автомобиля следующие классы частей автомобиля: Battery, Differential, Wheel. 
+Также добавьте в класс Car виртуальный обобщённый метод без реализации — ChangePart, 
+который будет принимать один параметр — newPart универсального типа.*/
 
-class Record<T1, T2>
+class Car<T1>
 {
-    public T1 Id;
-    public T2 Value;
-    public DateTime Date;
+    public T1 Engine;
+    public virtual void ChangePart<T2>(T2 newPart)  {}
 }
+class ElectricEngine {}
+class GasEngine {}
+class Battery {}
+class Differential {}
+class Wheel {}
 class Program
 {
     static void Main(string[] args)
