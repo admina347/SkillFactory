9d7a35fb6c18cf474a3877b05967cd8a942e299a
admina <it-mactep@mail.ru>
2022-09-06 14:40:01 +0500

    unit-7.6.12

--- Program.cs
+++ Program.cs
@@ -1,18 +1,36 @@
 ﻿using System;
-/*Задание 7.6.10
-Переименуйте универсальные параметры в более читаемые, например, TEngine и TPart.*/
+/*Задание 7.6.12
+С учётом полученных знаний по наследованию обобщений, дополните схему классов автомобиля, 
+добавив классы для электромобиля и бензинового — ElectricCar и GasCar.
 
-class Car<TEngine> where TEngine : Engine
+Подумайте, какой класс или классы можно сделать абстрактными. Сделайте абстрактными их и их члены (по возможности).*/
+
+class ElectricCar : Car<ElectricEngine>
 {
-    public virtual void ChangePart<TPart>(TPart newPart) where TPart : CarPart {}
+    public override void ChangePart<TPart>(TPart newPart)
+    {
+
+    }
 }
-class ElectricEngine : Engine {}
-class GasEngine : Engine {}
-abstract class Engine {}
-abstract class CarPart {}
-class Battery : CarPart {}
-class Differential : CarPart {}
-class Wheel : CarPart {}
+class GasCar : Car<GasEngine>
+{
+    public override void ChangePart<TPart>(TPart newPart)
+    {
+
+    }
+}
+abstract class Car<TEngine> where TEngine : Engine
+{
+    public TEngine Engine;
+    public abstract void ChangePart<TPart>(TPart newPart) where TPart : CarPart;
+}
+class ElectricEngine : Engine { }
+class GasEngine : Engine { }
+abstract class Engine { }
+abstract class CarPart { }
+class Battery : CarPart { }
+class Differential : CarPart { }
+class Wheel : CarPart { }
 class Program
 {
     static void Main(string[] args)
