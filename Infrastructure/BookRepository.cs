cd3ed06ad3d9130e5a6d83336915ecb0471510a0
admina <it-mactep@mail.ru>
2022-09-06 19:18:55 +0500

    unit-7.7 add repository

---
+++ Infrastructure/BookRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Store.DB;
+public class BookRepository
+{
+    private readonly Book[] books = new[]
+    {
+        new Book(1, "9785170882700", "Тёмная сторона", "Макс Фрай", "«Тёмная сторона» – это не метафора, а совершенно конкретное место, изнанка реальности. Тёмная сторона есть у любого города, деревни, леса и даже моря.",269m)
+    };
+    public Book[] GetAllBytitle(string titlePart)
+    {
+        throw new NotImplementedException();
+    }
+}
