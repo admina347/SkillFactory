60a835850e4a097b2e3bb5fdb8fe943c75023fb3
admina <it-mactep@mail.ru>
2022-09-09 12:56:19 +0500

    cart mod, code refactroing

---
+++ Data/Munu.cs
@@ -0,0 +1,34 @@
+namespace Store
+{
+    //Меню
+    class Menu
+    {
+        public static void ShowMainMenu()
+        {
+            Console.WriteLine("-Главное меню-");
+            Console.WriteLine("Посмотреть все введите all");
+            Console.WriteLine("Узанть о книге подробнее введите № книги");
+            Console.WriteLine("Добавить книгу в корзину введите +");
+            Console.WriteLine("Посомтреть товары в корзине введите - cart");
+            Console.WriteLine("Выход - q");
+        }
+        //Меню книги
+        public static void ShowBookMenu()
+        {
+            Console.WriteLine("-Каталог-");
+            Console.WriteLine("Посмотреть все введите all");
+            Console.WriteLine("Добавить книгу в корзину введите +");
+            Console.WriteLine("Вернуться в Главное меню #");
+            Console.WriteLine("Выход - q");
+        }
+        //Меню корзины
+        public static void ShowCartMenu()
+        {
+            Console.WriteLine("-Корзина-");
+            Console.WriteLine("Оформить заказ ввeдиет z");
+            Console.WriteLine("Удалить книгу из корзины введите -");            
+            Console.WriteLine("Вернуться в Главное меню - #");
+            Console.WriteLine("Выход - q");
+        }
+    }
+}
