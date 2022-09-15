c0b4414ed01fa17a994a2cf84cefc1e565796eb9
admina <it-mactep@mail.ru>
2022-09-15 13:49:14 +0500

    unit-8.3.2

--- Program.cs
+++ Program.cs
@@ -6,12 +6,20 @@ class FileWriter
     {
         string filePath = @"/home/admina/Документы/VSCode/repos/SkillFactory/Program.cs"; // Укажем путь
 
-        // Откроем файл и прочитаем его содержимое
-        using (StreamReader sr = File.OpenText(filePath))
+        var fileInfo = new FileInfo(filePath); // Создаем объект класса FileInfo.
+        //Открываем файл и читаем из него.
+        using (StreamReader sr = fileInfo.OpenText())
         {
             string str = "";
             while ((str = sr.ReadLine()) != null)
+            {
                 Console.WriteLine(str);
+            }
+        }
+        //записываем в него.
+        using (StreamWriter sw = fileInfo.AppendText())
+        {
+            sw.WriteLine("//Последнний запуск: {0}", DateTime.Now);
         }
     }
-}
+}//Последнний запуск: 15.09.2022 13:48:38
