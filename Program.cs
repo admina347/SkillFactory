daab2c2b414ea5cd78e974957273dc57bf8fe1f1
admina <it-mactep@mail.ru>
2022-09-15 14:23:37 +0500

    unit-8.4.2

--- Program.cs
+++ Program.cs
@@ -1,45 +1,43 @@
 ﻿using System;
 using System.IO;
- 
+
 class BinaryExperiment
 {
-   const string SettingsFileName = "/home/admina/Загрузки/BinaryFile.bin";
- 
-   static void Main()
-   {
+    const string SettingsFileName = "/home/admina/Загрузки/BinaryFile.bin";
 
-       // Считываем
-       ReadValues();
-   }
- 
+    static void Main()
+    {
+        // Пишем
+        WriteValues();
+        // Считываем
+        ReadValues();
+    }
 
- 
-   static void ReadValues()
-   {
-       float FloatValue;
-       string StringValue;
-       int IntValue;
-       bool BooleanValue;
- 
-       if (File.Exists(SettingsFileName))
-       {
-           // Создаем объект BinaryReader и инициализируем его возвратом метода File.Open.
-           using (BinaryReader reader = new BinaryReader(File.Open(SettingsFileName, FileMode.Open)))
-           {
-               // Применяем специализированные методы Read для считывания соответствующего типа данных.
-               //FloatValue = reader.ReadSingle();
-               StringValue = reader.ReadString();
-               //IntValue = reader.ReadInt32();
-               //BooleanValue = reader.ReadBoolean();
-           }
- 
-           Console.WriteLine("Из файла считано:");
- 
-           //Console.WriteLine("Дробь: " + FloatValue);
-           Console.WriteLine("Строка: " + StringValue);
-           //Console.WriteLine("Целое: " + IntValue);
-           //Console.WriteLine("Булево значение " + BooleanValue);
-       }
-       
-   }
+
+
+    static void ReadValues()
+    {
+        string StringValue;
+
+        if (File.Exists(SettingsFileName))
+        {
+            // Создаем объект BinaryReader и инициализируем его возвратом метода File.Open.
+            using (BinaryReader reader = new BinaryReader(File.Open(SettingsFileName, FileMode.Open)))
+            {
+                // Применяем специализированные методы Read для считывания соответствующего типа данных.
+                StringValue = reader.ReadString();
+            }
+            Console.WriteLine("Из файла считано:");
+            Console.WriteLine("Строка: " + StringValue);
+        }        
+    }
+    static void WriteValues()
+        {
+            // Создаем объект BinaryWriter и указываем, куда будет направлен поток данных
+            using (BinaryWriter writer = new BinaryWriter(File.Open(SettingsFileName, FileMode.Append)))
+            {
+                // записываем данные в разном формате
+                writer.Write($"Файл изменен {DateTime.Now} на компьютере c ОС {Environment.OSVersion}");
+            }
+        }
 }
