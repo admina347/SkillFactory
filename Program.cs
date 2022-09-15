b616ef301e8b642e25418a20a74e30c37536f06c
admina <it-mactep@mail.ru>
2022-09-15 11:28:47 +0500

    unit8.2.3

--- Program.cs
+++ Program.cs
@@ -17,11 +17,28 @@
                     newDirectory.Create();
 
                 Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+
+                Console.WriteLine($"Название каталога: {dirInfo.Name}");
+                Console.WriteLine($"Полное название каталога: {dirInfo.FullName}");
+                Console.WriteLine($"Время создания каталога: {dirInfo.CreationTime}");
+                Console.WriteLine($"Корневой каталог: {dirInfo.Root}");
+
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"/home/admina/newDirectory");
+                dirInfo.Delete(true); // Удаление со всем содержимым
+                Console.WriteLine("Каталог удален");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
         }
     }
 }
