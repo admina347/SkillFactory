5d0950e34bc720d0485ec9058dbf1b71d92320aa
admina <it-mactep@mail.ru>
2022-09-19 18:11:05 +0500

    unit-9.2.2

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
-﻿class Program
+﻿/* Создайте консольное решение, в котором реализуйте конструкцию Try/Catch/Finally 
+для обработки исключения ArgumentOutOfRangeException. 
+В случае исключения отобразите в консоль сообщение об ошибке. */
+class Program
 {
     static void Main(string[] args)
     {
-
+        try
+        {
+            Console.WriteLine("Блок try начал работу");
+            throw new ArgumentOutOfRangeException();
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Возникло исключение ArgumentOutOfRangeException");
+        }
+        finally
+        {
+            Console.WriteLine("Блок fianly отработал");
+        }
     }
 }
