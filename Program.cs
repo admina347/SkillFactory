30dab59671b78b77ae92d59d696c3fc8bf84aa62
admina <it-mactep@mail.ru>
2022-09-19 18:35:13 +0500

    unit-9.3.2

--- Program.cs
+++ Program.cs
@@ -1,22 +1,15 @@
-﻿/* Создайте консольное решение, в котором реализуйте конструкцию Try/Catch/Finally 
-для обработки исключения RankException. 
-В случае исключения отобразите в консоль тип исключения (через метод GetType()).*/
-class Program
+﻿public class Program 
 {
-    static void Main(string[] args)
-    {
-        try
-        {
-            Console.WriteLine("Блок try начал работу");
-            throw new RankException("Возникло исключение RankException");
-        }
-        catch (RankException ex)
-        {
-            Console.WriteLine(ex.GetType());
-        }
-        finally
-        {
-            Console.WriteLine("Блок fianly отработал");
-        }
-    }
+  public delegate int SubDelegate(int a, int b);
+  static void Main(string[] args) 
+  {
+    SubDelegate subDelegate = Sub;
+    int res = subDelegate.Invoke(10, 5);
+    Console.WriteLine(res);
+  }
+
+  static int Sub(int a, int b) 
+  {
+    return a - b;
+  }
 }
