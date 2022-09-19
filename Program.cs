1396977b9705e34c144ae2f492bc48df658fe01d
admina <it-mactep@mail.ru>
2022-09-19 18:07:15 +0500

    unit9 init

--- Program.cs
+++ Program.cs
@@ -1,49 +1,7 @@
-﻿using System;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-namespace Serialization
+﻿class Program
 {
-
-    // Описываем наш класс и помечаем его атрибутом для последующей сериализации   
-    [Serializable]
-    class Contact
+    static void Main(string[] args)
     {
-        public string Name { get; set; }
-        public long PhoneNumber { get; set; }
-        public string Email { get; set; }
 
-        public Contact(string name, long phoneNumber, string email)
-        {
-            Name = name;
-            PhoneNumber = phoneNumber;
-            Email = email;
-        }
-    }
-
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // объект для сериализации
-            var contact = new Contact("Ivan", 1234567890, "test@email.ru");
-            Console.WriteLine("Объект создан");
-
-            BinaryFormatter formatter = new BinaryFormatter();
-            // получаем поток, куда будем записывать сериализованный объект
-            using (var fs = new FileStream("myContact.dat", FileMode.OpenOrCreate))
-            {
-                formatter.Serialize(fs, contact);
-                Console.WriteLine("Объект сериализован");
-            }
-            // десериализация
-            using (var fs = new FileStream("myContact.dat", FileMode.OpenOrCreate))
-            {
-                var newContact = (Contact)formatter.Deserialize(fs);
-                Console.WriteLine("Объект десериализован");
-                Console.WriteLine($"Имя: {contact.Name} --- Телефон: {contact.PhoneNumber} Email: {contact.Email}");
-            }
-            Console.ReadLine();
-        }
     }
 }
