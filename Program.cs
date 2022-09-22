3a653f26f6c6c03759873a5af8abf5db188d114b
admina <it-mactep@mail.ru>
2022-09-22 18:58:11 +0500

    unit-9.6.1

--- Program.cs
+++ Program.cs
@@ -1,37 +1,43 @@
-﻿namespace CovAndContrPractices
+﻿namespace Unit961 //9.6.1 (HW-03)
 {
+    public class MyException : Exception
+    {
+        public MyException() { }
+
+        public MyException(string message) : base(message) { }
+    }
     class Program
     {
-        public delegate Parent HandlerMethod();
-        delegate void ChildInfo(Child child);
-
-        public static Parent ParentHandler()
-        {
-            return null;
-        }
-
-
-        public static Child ChildHandler()
-        {
-            return null;
-        }
-
         static void Main(string[] args)
         {
-            ChildInfo childInfo = GetParentInfo;
-
-            childInfo.Invoke(new Child());
-
-
-        }
-
-        public static void GetParentInfo(Parent p)
-        {
-            Console.WriteLine(p.GetType());
+            Exception[] exceptions = new Exception[]
+            {
+                new ArgumentException("Непустой аргумент, передаваемый в метод, является недопустимым."),
+                new DirectoryNotFoundException("Недопустимая часть пути к каталогу."),
+                new DriveNotFoundException("Диск недоступен или не существует."),
+                new FileNotFoundException("Файл не существует."),
+                new FormatException("Значение не находится в соответствующем формате для преобразования из строки методом преобразования, например Parse."),
+                new MyException("Мое исключение")
+            };
+            foreach (Exception e in exceptions)
+            {
+                try
+                {
+                    throw e;
+                }
+                catch (MyException ex)
+                {
+                    Console.WriteLine("{0}\n Перейдите по ссылке https://learn.microsoft.com/ru-ru/dotnet/csharp/fundamentals/exceptions/exception-handling", ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка: {0}", ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine();
+                }
+            }
         }
     }
-
-    class Parent { }
-
-    class Child : Parent {  }
 }
