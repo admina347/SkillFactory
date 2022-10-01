8ce13238c66c87d2809c3d8a65fcd696f198b1a2
admina <it-mactep@mail.ru>
2022-10-01 11:31:01 +0500

    unit10 init

--- Program.cs
+++ Program.cs
@@ -1,49 +1,10 @@
-﻿namespace EventPrictices //9.6.1 (HW-03)
+﻿namespace Unit10
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Human human = new Human();
-            IHumans rHumans;
-            rHumans = new Humans();
-            Human[] humans = rHumans.GetAll();
-            NumberReader numberReader = new NumberReader();
-            numberReader.NumberInputEvent += SortHuman;
-            try
-            {
-                numberReader.Read(humans);
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Введено не корректное значение");
-            }
-            catch (IndexOutOfRangeException)
-            {
-                Console.WriteLine("Введеное значение находится за пределами границ массива или коллекции.");
-            }
-        }
 
-        static void SortHuman(Human[] humans, int number)
-        {
-            Console.WriteLine();
-            switch (number)
-            {
-                case 1:
-                    {
-                        Array.Sort(humans, (x, y) => String.Compare(x.LastName, y.LastName));     //Сортировка
-                        Console.WriteLine("Сортировка А-Я");
-                        Humans.ShowAll(humans);
-                        break;
-                    }
-                case 2:
-                    {
-                        Array.Sort(humans, (x, y) => String.Compare(y.LastName, x.LastName));     //Сортировка
-                        Console.WriteLine("Сортировка Я-А");
-                        Humans.ShowAll(humans);
-                        break;
-                    }
-            }
         }
     }
 }
