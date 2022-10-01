e065134eedb0bb200f1008ff3134e19c39a2fc3c
admina <it-mactep@mail.ru>
2022-10-01 11:32:53 +0500

    unit10.2.2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
+            Writer writer = new Writer();
 
+            ((IWriter)writer).Write();
         }
+    }
+
+    public class Writer : IWriter
+    {
+        void IWriter.Write()
+        {
+            Console.WriteLine("Write");
+        }
+    }
+
+    public interface IWriter
+    {
+        void Write();
     }
 }
