2ee1687c84d8850aec4320ece41f148d051d2daa
admina <it-mactep@mail.ru>
2022-10-21 18:09:19 +0500

    unit-13.5.8

--- Program.cs
+++ Program.cs
@@ -1,26 +1,58 @@
-﻿using System.Collections.Generic;
- 
-namespace CountWords
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace StackTest
 {
-   class Program
-   {
-       static void Main(string[] args)
-       {
-           // Сохраняем предложение в строку
-           var sentence =
-               "Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
-          
-           // сохраняем в массив char
-           var characters = sentence.ToCharArray();
-          
-           var symbols = new HashSet<char>();
-     
-           // добавляем во множество. Сохраняются только неповторяющиеся символы
-           foreach (var symbol in characters)
-               symbols.Add(symbol);
-          
-           // Выводим результат
-           Console.WriteLine(symbols.Count);
-       }
-   }
+    class Program
+    {
+        // объявим потокобезопасную очередь (полностью идентична обычной очереди, но
+        // позволяет безопасный доступ
+        // из разных потоков)
+        public static ConcurrentQueue<string> words = new ConcurrentQueue<string>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово и нажмите Enter, чтобы добавить его в очередь.");
+            Console.WriteLine();
+
+            //  запустим обработку очереди в отдельном потоке
+            StartQueueProcessing();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                // если введена нужная нам команда - смотрим, кто крайний в очереди
+                if (input == "peek")
+                {
+                    if (words.TryPeek(out var elem))
+                        Console.WriteLine(elem);
+                }
+                else
+                {
+                    // если не введена - ставим элемент в очередь, как и обычно
+                    words.Enqueue(input);
+                }
+            }
+        }
+
+        // метод, который обрабатывает и разбирает нашу очередь в отдельном потоке
+        // ( для выполнения задания изменять его не нужно )
+        static void StartQueueProcessing()
+        {
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+
+                while (true)
+                {
+                    Thread.Sleep(5000);
+                    if (words.TryDequeue(out var element))
+                        Console.WriteLine("======>  " + element + " прошел очередь");
+                }
+
+            }).Start();
+        }
+    }
 }
