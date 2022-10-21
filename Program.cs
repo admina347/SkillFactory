23a3463d6bcfb914a8cd06af44638b3427b995b3
admina <it-mactep@mail.ru>
2022-10-21 18:26:55 +0500

    unit-13.4.5

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace PhoneBook
 {
     class Program
     {
-        //  Объявим  простой  словарь
-        private static Dictionary<string, Contact> PhoneBook = new Dictionary<String, Contact>()
+        //  Объявим  сортированный  словарь
+        private static SortedDictionary<string, Contact> SortedPhoneBook = new SortedDictionary<String, Contact>()
         {
             ["Игорь"] = new Contact(79990000000, "igor@example.com"),
             ["Андрей"] = new Contact(79990000001, "andrew@example.com"),
@@ -19,10 +19,10 @@ namespace PhoneBook
             var watchTwo = Stopwatch.StartNew();
 
             // Выполним вставку
-            PhoneBook.TryAdd("Диана", new Contact(79160000002, "diana@example.com"));
+            SortedPhoneBook.TryAdd("Диана", new Contact(79160000002, "diana@example.com"));
 
             // Выведем результат
-            Console.WriteLine($"Вставка в  словарь: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+            Console.WriteLine($"Вставка в сортированный словарь: {watchTwo.Elapsed.TotalMilliseconds}  мс");
         }
     }
 
