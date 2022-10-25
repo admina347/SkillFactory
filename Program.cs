ad394b77f37049486a2ee570c370dfdab39e4cd9
admina <it-mactep@mail.ru>
2022-10-25 13:32:14 +0500

    unit-14.1.1

--- Program.cs
+++ Program.cs
@@ -1,19 +1,43 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace module14
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            var objects = new List<object>()
+            // Добавим Россию с её городами
+            var russianCities = new List<City>();
+            russianCities.Add(new City("Москва", 11900000));
+            russianCities.Add(new City("Санкт-Петербург", 4991000));
+            russianCities.Add(new City("Волгоград", 1099000));
+            russianCities.Add(new City("Казань", 1169000));
+            russianCities.Add(new City("Севастополь", 449138));
+
+            var bigCities = from russianCity in russianCities
+                            where russianCity.Population > 1000000
+                            orderby russianCity.Population descending
+                            select russianCity;
+
+            foreach (var bigCity in bigCities)
+                Console.WriteLine(bigCity.Name + " - " + bigCity.Population);
+
+        }
+
+        // Создадим модель класс для города
+        public class City
+        {
+            public City(string name, long population)
             {
-                1,
-                "Сергей ",
-                "Андрей ",
-                300,
-            };
-            foreach (var stringObject in objects.Where(o => o is string).OrderBy(o => o))
-                Console.WriteLine(stringObject);
+                Name = name;
+                Population = population;
+            }
+
+            public string Name { get; set; }
+            public long Population { get; set; }
         }
     }
 }
