1ed56e922e99cf821c99417605842a8c11ffcf0b
admina <it-mactep@mail.ru>
2022-10-25 13:41:56 +0500

    unit-14.1.1-2

--- Program.cs
+++ Program.cs
@@ -17,14 +17,15 @@ namespace module14
             russianCities.Add(new City("Казань", 1169000));
             russianCities.Add(new City("Севастополь", 449138));
 
-            var bigCities = from russianCity in russianCities
-                            where russianCity.Population > 1000000
-                            orderby russianCity.Population descending
-                            select russianCity;
-
+            var bigCities = russianCities.Where(c => c.Population > 1000000)
+  .OrderByDescending(c => c.Population);
             foreach (var bigCity in bigCities)
                 Console.WriteLine(bigCity.Name + " - " + bigCity.Population);
 
+            var shortNameCities = russianCities.Where(city => city.Name.Length <= 10) // выборка городов с коротким именем
+                                .OrderBy(city => city.Name.Length);  // сортировка по длине имени
+            foreach (var city in shortNameCities)
+                Console.WriteLine(city.Name);
         }
 
         // Создадим модель класс для города
