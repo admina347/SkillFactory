890a33d532371ca5e6438c286354d2606cba5e8e
admina <it-mactep@mail.ru>
2022-10-26 13:13:52 +0500

    unit-14.2.4

--- Program.cs
+++ Program.cs
@@ -8,27 +8,43 @@ namespace module14
     {
         static void Main(string[] args)
         {
-            // Наш список студентов
-            List<Student> students = new List<Student>
+            // Список студентов
+            var students = new List<Student>
             {
                 new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
                 new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
-                new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
-                new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
+                new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }}
             };
-            var anketa = from s in students
-                         where s.Age < 27 // берем тех, кто младше 27
-                         let birthYear = DateTime.Now.Year - s.Age // Вычисляем год рождения
-                                                                   // спроецируем в новую сущеность анкеты
-                         select new Application()
-                         {
-                             Name = s.Name,
-                             YearOfBirth = birthYear
-                         };
-            //  вывод
-            foreach (var stud in anketa)
-                Console.WriteLine(stud.Name + ", " + stud.YearOfBirth);
+
+            // Список курсов
+            var coarses = new List<Coarse>
+            {
+                new Coarse {Name="Язык программирования C#", StartDate = new DateTime(2020, 12, 20)},
+                new Coarse {Name="Язык SQL и реляционные базы данных", StartDate = new DateTime(2020, 12, 15)},
+            };
+            var studentsWithCoarses = from stud in students
+                                      where stud.Age < 29 // берем всех студентов младше 29
+                                      where stud.Languages.Contains("английский") // ищем тех, у кого в списке языков есть английский
+                                      let birthYear = DateTime.Now.Year - stud.Age // Вычисляем год рождения
+                                      from coarse in coarses
+                                      where coarse.Name.Contains("C#") // теперь выбираем только курс по C#
+                                      select new // выборка в новую сущность
+                                      {
+                                          Name = stud.Name,
+                                          BirthYear = birthYear,
+                                          CoarseName = coarse.Name
+                                      };
+
+            // выведем результат
+            foreach (var stud in studentsWithCoarses)
+                Console.WriteLine($"Студент {stud.Name} ({stud.BirthYear}) добавлен курс {stud.CoarseName}");
         }
+    }
+
+    internal class Coarse
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
     }
 
     public class Application
