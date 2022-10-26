0d000038cb0b83a4cdbe77a158ae687bbe58bb86
admina <it-mactep@mail.ru>
2022-10-26 13:07:00 +0500

    unit-14.2.3

--- Program.cs
+++ Program.cs
@@ -8,17 +8,39 @@ namespace module14
     {
         static void Main(string[] args)
         {
-            string[] words = { "Обезьяна", "Лягушка", "Кот", "Собака", "Черепаха" };
-            // проекция в анонимный тип
-            var sWords = words.Select(w => new
+            // Наш список студентов
+            List<Student> students = new List<Student>
             {
-                Name = w,
-                Length = w.Length
-            })
-            .OrderBy(word => word.Length);
-            // выводим
-            foreach (var word in sWords)
-                Console.WriteLine($"{word.Name} - {word.Length} букв");
+                new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
+                new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
+                new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
+                new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
+            };
+            var anketa = from s in students
+                         where s.Age < 27 // берем тех, кто младше 27
+                         let birthYear = DateTime.Now.Year - s.Age // Вычисляем год рождения
+                                                                   // спроецируем в новую сущеность анкеты
+                         select new Application()
+                         {
+                             Name = s.Name,
+                             YearOfBirth = birthYear
+                         };
+            //  вывод
+            foreach (var stud in anketa)
+                Console.WriteLine(stud.Name + ", " + stud.YearOfBirth);
         }
+    }
+
+    public class Application
+    {
+        public string Name { get; set; }
+        public int YearOfBirth { get; set; }
+    }
+
+    internal class Student
+    {
+        public string Name { get; internal set; }
+        public int Age { get; internal set; }
+        public List<string> Languages { get; internal set; }
     }
 }
