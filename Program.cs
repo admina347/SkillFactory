fd888546ec2fd6a90075574d3b96a133a0c6a43a
admina <it-mactep@mail.ru>
2022-10-31 11:34:20 +0500

    unit-15.1.4

--- Program.cs
+++ Program.cs
@@ -2,69 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace module14
+namespace module15;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            //  создаём пустой список с типом данных Contact
-            var phoneBook = new List<Contact>();
-
-            // добавляем контакты
-            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
-            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
-            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
-            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
-            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
-            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
-
-            //
-            while (true)
-            {
-                // Читаем введенный с консоли символ
-                var input = Console.ReadKey().KeyChar;
-
-                // проверяем, число ли это
-                var parsed = Int32.TryParse(input.ToString(), out int pageNumber);
-
-                // если не соответствует критериям - показываем ошибку
-                if (!parsed || pageNumber < 1 || pageNumber > 3)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Страницы не существует");
-                }
-                // если соответствует - запускаем вывод
-                else
-                {
-                    // пропускаем нужное количество элементов и берем 2 для показа на странице
-                    var pageContent = phoneBook.Skip((pageNumber - 1) * 2).Take(2)
-                    .OrderBy(phoneBook => phoneBook.Name)   //отсортированы сперва по имени, а затем по фамилии.
-                    .ThenBy(phoneBook => phoneBook.LastName);
-                    Console.WriteLine();
-
-                    // выводим результат
-                    foreach (var entry in pageContent)
-                        Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
-
-                    Console.WriteLine();
-                }
-            }
-        }
+        Console.WriteLine(CountCommon("one", "two"));
     }
-    public class Contact // модель класса
+    static int CountCommon(string word1, string word2)
     {
-        public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
-        {
-            Name = name;
-            LastName = lastName;
-            PhoneNumber = phoneNumber;
-            Email = email;
-        }
-
-        public String Name { get; }
-        public String LastName { get; }
-        public long PhoneNumber { get; }
-        public String Email { get; }
+        var amount = word1.Intersect(word2)//   ищем пересечение
+ .Count(); // считаем количество
+        return amount;
     }
 }
