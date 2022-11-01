79f3172d14e2c3a51fd483f301044f22c2754aaf
admina <it-mactep@mail.ru>
2022-11-01 15:13:23 +0500

    unit-15.5.4

--- Program.cs
+++ Program.cs
@@ -6,65 +6,36 @@ namespace module15;
 
 class Program
 {
-    //   статическая переменная для хранения данных в памяти
-    public static List<int> Numbers = new List<int>();
     static void Main(string[] args)
     {
-        var departments = new List<Department>()
-        {
-            new Department() {Id = 1, Name = "Программирование"},
-            new Department() {Id = 2, Name = "Продажи"}
-        };
+        /* //  Подготовим тестовые данные
+        var names = new List<string>() { "Вася", "Вова", "Петя", "Андрей" };
 
-        var employees = new List<Employee>()
-        {
-            new Employee() { DepartmentId = 1, Name = "Инна", Id = 1},
-            new Employee() { DepartmentId = 1, Name = "Андрей", Id = 2},
-            new Employee() { DepartmentId = 2, Name = "Виктор ", Id = 3},
-            new Employee() { DepartmentId = 3, Name = "Альберт ", Id = 4},
-        };
-        var result2 = departments.GroupJoin(employees, // передаем в качестве параметра вторую коллекцию
-            dep => dep.Id, // указываем общее свойство для второй коллекции
-            emp => emp.DepartmentId, // указываем общее свойство для первой коллекции
-            (d, emps) =>
-            new // проекция в новый тип
-            {
-                DepName = d.Name,
-                Emps = emps.Select(d => d.Name)
-            });
+        // Подготовим тестовую выборку (без ToArray())
+        var experiment = names.Where(name => name.StartsWith("В"));
 
-        // Вывод:
-        foreach (var team in result2)
-        {
-            Console.WriteLine(team.DepName + ":");
+        // уберем несколько элементов уже после выборки (если она выполняется отложено, то они в неё не попадут)
+        names.Remove("Вася");
+        names.Remove("Вова");
 
-            foreach (string em in team.Emps)
-                Console.WriteLine(em);
+        // обратимся к выборке в цикле foreach
+        foreach (var word in experiment)
+            Console.WriteLine(word); */
 
-            Console.WriteLine();
-        }
+        //  Снова возьмем те же тестовые данные
+        var names = new List<string>() { "Вася", "Вова", "Петя", "Андрей" };
+
+        // Теперь добавим ToArray() в конце того же самого LINQ-запроса
+        var experiment = names.Where(name => name.StartsWith("В")).ToArray();
+
+        // Также уберем несколько элементов
+        names.Remove("Вася");
+        names.Remove("Вова");
+
+        // обратимся к выборку в цикле foreach
+        foreach (var word in experiment)
+            Console.WriteLine(word);
 
     }
 
-}
-
-internal class Employee
-{
-    public Employee()
-    {
-    }
-
-    public int DepartmentId { get; set; }
-    public string Name { get; set; }
-    public int Id { get; set; }
-}
-
-internal class Department
-{
-    public Department()
-    {
-    }
-
-    public int Id { get; set; }
-    public string Name { get; set; }
 }
